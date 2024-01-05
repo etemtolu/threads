f71704932d4bee5d922350eeb72acd3038c586a7
etemtolu <42868963+etemtolu@users.noreply.github.com>
2024-01-05 22:33:27 +0300

    first commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+async function Page() {
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p>Complate your profile now to use Threads</p>
+      <section>
+        
+      </section>
+    </main>
+  );
+}
+export default Page;
